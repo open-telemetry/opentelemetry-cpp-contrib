87b958768b9046db40ac54aa881e5086fe22f70c
Siim Kallas <siimkallas@gmail.com>
2021-03-29 15:21:08 +0300

    remove unused CI files, use system nginx

--- instrumentation/nginx/nginx.cmake
+++ instrumentation/nginx/nginx.cmake
@@ -1,9 +1,21 @@
 include(ExternalProject)
 
-set(NGINX_VER "1.18.0" CACHE STRING "Nginx version to compile against")
+if (NOT NGINX_VERSION)
+  find_program(NGINX_BIN nginx REQUIRED)
+  execute_process(COMMAND ${NGINX_BIN} -v
+    ERROR_VARIABLE NGINX_VERSION_STRING
+  )
+ 
+  string(REGEX MATCH "[0-9]+\\.\[0-9]+\\.[0-9]+" NGINX_VER ${NGINX_VERSION_STRING})
+else()
+  set(NGINX_VER "1.18.0")
+endif()
+
+set(NGINX_VERSION ${NGINX_VER} CACHE STRING "Nginx version to compile against")
+message(STATUS "Using nginx ${NGINX_VERSION}")
 
 ExternalProject_Add(project_nginx
-  URL "http://nginx.org/download/nginx-${NGINX_VER}.tar.gz"
+  URL "http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz"
   PREFIX "nginx"
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ./configure --with-compat
