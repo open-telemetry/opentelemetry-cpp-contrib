1b1c176b9072bdb0d6c6fec4c0604d796463c806
Tomasz Rojek <trojek@splunk.com>
2021-04-13 14:47:22 +0200

    Fix script so it can be run from any directory

--- instrumentation/httpd/create-otel-load.sh
+++ instrumentation/httpd/create-otel-load.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # create configuration file for httpd (Apache)
 cat << EOF > opentelemetry.load
 # C++ Standard library
 LoadFile /usr/lib/x86_64-linux-gnu/libstdc++.so.6
 
-LoadModule otel_module $PWD/bazel-out/k8-opt/bin/otel.so
+LoadModule otel_module $SCRIPT_DIR/bazel-out/k8-opt/bin/otel.so
 EOF
