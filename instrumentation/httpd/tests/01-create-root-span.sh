03acc95f81506d17e5a1537bc322c5040df2d42c
Tomasz Rojek <trojek@splunk.com>
2021-05-12 17:15:36 +0200

    Fix tests to match how attributes are presented (it was broken by #632 commit 179a7f40)

--- instrumentation/httpd/tests/01-create-root-span.sh
+++ instrumentation/httpd/tests/01-create-root-span.sh
@@ -28,22 +28,12 @@ check_results() {
    [ "`getSpanField span_id`" != "`getSpanField parent_span_id`" ] || fail "Bad span: span.id same as parent span.id"
 
    echo Checking span attributes
-   declare -A SPAN_ATTRS
-   # transforms "http.method: GET, http.flavor: http, ..." into SPAN_ATTRS[http.method] = GET
-   IFS=',' read -ra my_array <<< "`getSpanField attributes`"
-   for i in "${my_array[@]}"; do
-     TMP_KEY=${i%%:*}
-     TMP_KEY=${TMP_KEY# }
-     TMP_VAL=${i##*:}
-     TMP_VAL=${TMP_VAL:1}
-     SPAN_ATTRS[${TMP_KEY}]="${TMP_VAL}"
-   done
 
-   [[ "${SPAN_ATTRS[http.method]}" == "GET" ]] || fail "Bad span attribiute http.method ${SPAN_ATTRS[http.method]}"
-   [[ "${SPAN_ATTRS[http.target]}" == "/" ]] || fail "Bad span attribiute http.target ${SPAN_ATTRS[http.target]}"
-   [[ "${SPAN_ATTRS[http.status_code]}" == "200" ]] || fail "Bad span attribiute http.status_code ${SPAN_ATTRS[http.status_code]}"
-   [[ "${SPAN_ATTRS[http.flavor]}" == "1.1" ]] || fail "Bad span attribiute http.flavor ${SPAN_ATTRS[http.flavor]}"
-   [[ "${SPAN_ATTRS[http.scheme]}" == "http" ]] || fail "Bad span attribiute http.scheme ${SPAN_ATTRS[http.scheme]}"
+   [[ "`getSpanAttr 'http.method'`" == "GET" ]] || fail "Bad span attribiute http.method ${SPAN_ATTRS[http.method]}"
+   [[ "`getSpanAttr 'http.target'`" == "/" ]] || fail "Bad span attribiute http.target ${SPAN_ATTRS[http.target]}"
+   [[ "`getSpanAttr 'http.status_code'`" == "200" ]] || fail "Bad span attribiute http.status_code ${SPAN_ATTRS[http.status_code]}"
+   [[ "`getSpanAttr 'http.flavor'`" == "1.1" ]] || fail "Bad span attribiute http.flavor ${SPAN_ATTRS[http.flavor]}"
+   [[ "`getSpanAttr 'http.scheme'`" == "http" ]] || fail "Bad span attribiute http.scheme ${SPAN_ATTRS[http.scheme]}"
 }
 
 run $@
