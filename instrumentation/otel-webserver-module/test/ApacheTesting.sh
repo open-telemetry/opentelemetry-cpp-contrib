88569c0172e2f0dfff22b2e5231f23b3d94c4861
Kumar Pratyush <95214718+kpratyus@users.noreply.github.com>
2022-04-22 11:47:46 +0530

    Centos7 Suport Dockerfile changes (#151)
    * Centos7 Dockerfile changes
    
    * Removing comments
    
    * Removing extra comments
    
    * Centos6 Docker file variable add
    
    * Add variable for Centos7
    
    * Centos6 Boost dependency update
    
    * Boost update Centos7
    
    * Changing if else conditions
    
    * Elif change

--- instrumentation/otel-webserver-module/test/ApacheTesting.sh
+++ instrumentation/otel-webserver-module/test/ApacheTesting.sh
@@ -7,9 +7,10 @@
 targetSystem=$1
 
 OTelApacheModule="/opt/opentelemetry-webserver-sdk/WebServerModule/Apache/libmod_apache_otel22.so"
-if [ $targetSystem = "ubuntu" ]; then
+if [ $targetSystem = "ubuntu" ] || [ $targetSystem = "centos7" ]; then
 	OTelApacheModule="/opt/opentelemetry-webserver-sdk/WebServerModule/Apache/libmod_apache_otel.so"
 fi
+
 
 tar -xf ../build/opentelemetry-webserver-sdk-x64-linux.tgz -C /opt
 
@@ -64,6 +65,8 @@ ApacheModuleSegmentParameter 2
 targetConfFile='/etc/httpd/conf.d/opentelemetry_module.conf'
 if [ $targetSystem = "ubuntu" ]; then
 	targetConfFile='/etc/apache2/opentelemetry_module.conf'
+elif [ $targetSystem = "centos7" ]; then
+	targetConfFile='/etc/httpd/conf/opentelemetry_module.conf'
 fi
 
 rm -f $targetConfFile
@@ -72,6 +75,11 @@ cp -f /opt/opentelemetry_module.conf $targetConfFile
 if [ $targetSystem = "ubuntu" ]; then
 	if ! grep -Fxq "Include opentelemetry_module.conf" /etc/apache2/apache2.conf; then
 		echo 'Include opentelemetry_module.conf' >> /etc/apache2/apache2.conf
+	fi
+
+elif [ $targetSystem = "centos7" ]; then
+	if ! grep -Fxq "Include opentelemetry_module.conf" /etc/httpd/conf/httpd.conf; then
+		echo 'Include opentelemetry_module.conf' >> /etc/httpd/conf/httpd.conf
 	fi
 fi
 
