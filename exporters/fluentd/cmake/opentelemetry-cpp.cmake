0f215e976d23be192450597ff2f1be0de18b3374
Lalit <labhas@microsoft.com>
2022-05-05 19:58:37 -0700

    fix-tag

--- exporters/fluentd/cmake/opentelemetry-cpp.cmake
+++ exporters/fluentd/cmake/opentelemetry-cpp.cmake
@@ -1,5 +1,5 @@
 if("${opentelemetry-cpp-tag}" STREQUAL "")
-    set(opentelemetry-cpp-tag "v1.3.0")
+    set(opentelemetry-cpp-tag "75c2a8f7a6bf81d9799e76804473609cc236b7be")
 endif()
 function(target_create _target _lib)
   add_library(${_target} STATIC IMPORTED)
@@ -15,7 +15,7 @@ function(build_opentelemetry)
   set(opentelemetry_CMAKE_ARGS -DCMAKE_POSITION_INDEPENDENT_CODE=ON
           -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
           -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
-	 		    -DWITH_LOGS_PREVIEW=ON
+          -DWITH_LOGS_PREVIEW=ON
           -DBUILD_TESTING=OFF
           -DWITH_EXAMPLES=OFF)
 
