dfac7b1c310d9207ad11be45e074c6d9ebeba565
Lalit <labhas@microsoft.com>
2022-08-28 22:03:38 -0700

    changes

--- exporters/geneva/include/opentelemetry/exporters/geneva/metrics/exporter.h
+++ exporters/geneva/include/opentelemetry/exporters/geneva/metrics/exporter.h
@@ -16,6 +16,13 @@ namespace geneva
 {
 namespace metrics
 {
+constexpr size_t kBufferSize = 65360; // the maximum ETW payload (inclusive)
+constexpr size_t kMaxDimensionNameSize = 256;
+constexpr size_t kMaxDimensionValueSize = 1024;
+constexpr size_t kBinaryHeaderSize = 4 ; // event_id (2) + body_length (2)
+constexpr size_t kMetricPayloadSize = 24 ; // count_dimension (2)  + reserverd_word (2) + reserverd_dword(4) + timestamp_utc (8) + metric_data (8)
+constexpr size_t kExternalPayloadSize = 40 ; // count_dimension (2) + reserverd_word (2) + count (4) + timestamp_utc (8) + metric_data_sum (8) + metric_data_min(8) + metric_data_max(8)
+
 /**
  * The Geneva metrics exporter exports metrics data to Geneva 
  */
@@ -44,7 +51,79 @@ private:
     bool is_shutdown_ = false;
     mutable opentelemetry::common::SpinLockMutex lock_;
     std::unique_ptr<DataTransport> data_transport_;
+    
+    // metrics storage
+    unsigned char  buffer_non_histogram_[kBufferSize];
+    unsigned char  buffer_histogram_[kBufferSize];
+    uint64_t buffer_index_non_histogram_;
+    uint64_t buffer_index_histogram_;
+
+    size_t InitializeBufferForNonHistogramData();
+    size_t InitiaizeBufferForHistogramData();
+    size_t SerializeNonHistogramMetrics(sdk::metrics::AggregationType, const sdk::metrics::PointType &,  common::SystemTimestamp, std::string, const sdk::metrics::PointAttributes& );
+    size_t SerializeHistogramMetrics(sdk::metrics::AggregationType, const sdk::metrics::PointType & ,  common::SystemTimestamp, std::string, const sdk::metrics::PointAttributes&  );
 };
+
+template <class T>
+static void SerializeInt(unsigned char *buffer, size_t &index, T value) {
+    *(reinterpret_cast<T*>(buffer + index)) = value;
+    index+= sizeof(T);     
+}
+
+static void SerializeString(unsigned char *buffer, size_t &index, const std::string& str)
+{
+    auto size = str.size();
+    SerializeInt<uint16_t>(buffer, index, static_cast<uint16_t>(size));
+    index += sizeof(uint16_t);
+
+    if (size > 0 ){
+        memcpy(buffer + index, str.c_str(), size);
+    }
+    index += size;
+}
+
+static std::string AttributeValueToString(
+    const opentelemetry::sdk::common::OwnedAttributeValue &value)
+{
+  std::string result;
+  if (nostd::holds_alternative<bool>(value))
+  {
+    result = nostd::get<bool>(value) ? "true" : "false";
+  }
+  else if (nostd::holds_alternative<int>(value))
+  {
+    result = std::to_string(nostd::get<int>(value));
+  }
+  else if (nostd::holds_alternative<int64_t>(value))
+  {
+    result = std::to_string(nostd::get<int64_t>(value));
+  }
+  else if (nostd::holds_alternative<unsigned int>(value))
+  {
+    result = std::to_string(nostd::get<unsigned int>(value));
+  }
+  else if (nostd::holds_alternative<uint64_t>(value))
+  {
+    result = std::to_string(nostd::get<uint64_t>(value));
+  }
+  else if (nostd::holds_alternative<double>(value))
+  {
+    result = std::to_string(nostd::get<double>(value));
+  }
+  else if (nostd::holds_alternative<std::string>(value))
+  {
+    result = nostd::get<std::string>(value);
+  }
+  else
+  {
+
+    LOG_WARN(
+        "[Geneva Metrics Exporter] AttributeValueToString - "
+        " Nested attributes not supported - ignored");
+  }
+  return result;
+}
+
 }
 }
 }
--- exporters/geneva/src/exporter.cc
+++ exporters/geneva/src/exporter.cc
@@ -17,13 +17,19 @@ namespace geneva
 namespace metrics
 {
     Exporter::Exporter(const ExporterOptions &options):options_(options), 
-      connection_string_parser_(options_.connection_string), data_transport_{nullptr}
+      connection_string_parser_(options_.connection_string), data_transport_{nullptr},
+      buffer_index_histogram_(0), buffer_index_non_histogram_(0)
     {
       if (connection_string_parser_.IsValid()){
         if (connection_string_parser_.transport_protocol_ == TransportProtocol::kUNIX){
           data_transport_ = std::unique_ptr<DataTransport>(new UnixDomainSocketDataTransport(options_.connection_string));
         }
       }
+      // Initialize non-histogram buffer
+      buffer_index_non_histogram_=  InitializeBufferForNonHistogramData();
+
+      // Initialize histogram buffer
+      buffer_index_histogram_= InitiaizeBufferForHistogramData();
     }
 
     sdk::metrics::AggregationTemporality Exporter::GetAggregationTemporality(
@@ -61,10 +67,17 @@ opentelemetry::sdk::common::ExportResult Exporter::Export(
             for (auto &point_data_with_attributes : metric_data.point_data_attr_)
             if (nostd::holds_alternative<sdk::metrics::SumPointData>(point_data_with_attributes.point_data))
             {
-                
+              SerializeNonHistogramMetrics(sdk::metrics::AggregationType::kSum, point_data_with_attributes.point_data, metric_data.end_ts, metric_data.instrument_descriptor.name_, point_data_with_attributes.attributes);
+            }
+            else if (nostd::holds_alternative<sdk::metrics::LastValuePointData>(point_data_with_attributes.point_data))
+            {
+              SerializeNonHistogramMetrics(sdk::metrics::AggregationType::kLastValue, point_data_with_attributes.point_data, metric_data.end_ts, metric_data.instrument_descriptor.name_, point_data_with_attributes.attributes);
+
+            } else if (nostd::holds_alternative<sdk::metrics::HistogramPointData>(point_data_with_attributes.point_data))
+            {
+              SerializeHistogramMetrics(sdk::metrics::AggregationType::kLastValue, point_data_with_attributes.point_data, metric_data.end_ts, metric_data.instrument_descriptor.name_,point_data_with_attributes.attributes);
             }
         }
-
     }
     return opentelemetry::sdk::common::ExportResult::kSuccess;
 }
@@ -79,6 +92,68 @@ bool Exporter::Shutdown(std::chrono::microseconds timeout) noexcept
   const std::lock_guard<opentelemetry::common::SpinLockMutex> locked(lock_);
   is_shutdown_ = true;
   return true;
+}
+
+
+size_t Exporter::InitializeBufferForNonHistogramData()
+{
+  // The buffer format is as follows:
+  // -- BinaryHeader
+  // -- MetricPayload
+  // -- Variable length content
+
+  // Leave enough space for the header and fixed payload
+  auto bufferIndex =  kBinaryHeaderSize + kMetricPayloadSize;
+  SerializeString(buffer_non_histogram_, bufferIndex, connection_string_parser_.account_);
+  SerializeString(buffer_non_histogram_, bufferIndex, connection_string_parser_.namespace_);
+  return bufferIndex;
+}
+
+size_t Exporter::InitiaizeBufferForHistogramData()
+{
+  // The buffer format is as follows:
+  // -- BinaryHeader
+  // -- ExternalPayload
+  // -- Variable length content
+
+  // Leave enough space for the header and fixed payload
+  auto bufferIndex =  kBinaryHeaderSize + kExternalPayloadSize;
+  SerializeString(buffer_non_histogram_, bufferIndex, connection_string_parser_.account_);
+  SerializeString(buffer_non_histogram_, bufferIndex, connection_string_parser_.namespace_);
+  return bufferIndex;
+}
+
+size_t Exporter::SerializeNonHistogramMetrics(sdk::metrics::AggregationType agg_type, const sdk::metrics::PointType& point_data,  common::SystemTimestamp ts, std::string metric_name, const sdk::metrics::PointAttributes& attributes)
+{
+  auto bufferIndex = buffer_index_non_histogram_;
+  SerializeString(buffer_non_histogram_, bufferIndex, metric_name);
+  for (const auto &kv: attributes ){
+    if (kv.first.size() > kMaxDimensionNameSize ) {
+      LOG_WARN("Dimension name limit overflow: %s", kv.first);
+      continue;
+    }
+    SerializeString(buffer_non_histogram_, bufferIndex, kv.first);
+  }
+  for (const auto &kv: attributes){
+    auto attr_value = AttributeValueToString(kv.second);
+    SerializeString(buffer_non_histogram_, bufferIndex, attr_value);
+  }
+  // length zero for auto-pilot
+  SerializeInt<uint16_t>(buffer_non_histogram_, bufferIndex, 0);
+
+  // get final size of payload
+  uint16_t bodyLength = bufferIndex - kBufferSize;
+
+  //Add rest of the fields in front of buffer
+  bufferIndex  = k
+  SerializeNonHistogramMetrics()
+
+
+
+
+
+
+
 }
 
 }
