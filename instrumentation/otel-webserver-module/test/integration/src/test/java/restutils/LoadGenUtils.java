7eb21c898c6a72bf6b4e7d637a9bbc240aa1ff63
debasis9dash <debasis.sedge@gmail.com>
2022-09-06 20:25:08 +0530

    changes for debug

--- instrumentation/otel-webserver-module/test/integration/src/test/java/restutils/LoadGenUtils.java
+++ instrumentation/otel-webserver-module/test/integration/src/test/java/restutils/LoadGenUtils.java
@@ -6,13 +6,7 @@ public class LoadGenUtils {
 
     public void generateLoad(String Uri, int count) {
         while(count > 0){
-            if(count < 3){
-                RestAssured.given().get(Uri).then().log()
-                        .all();
-            } else {
-                RestAssured.given().get(Uri);
-            }
-
+            RestAssured.given().get(Uri);
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
