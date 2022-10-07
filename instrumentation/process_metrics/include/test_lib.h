dbe6084db6745a1fbdf23072b492dea2d01934c7
Lalit <labhas@microsoft.com>
2022-10-06 23:26:06 -0700

    more changes

--- instrumentation/process_metrics/include/test_lib.h
+++ instrumentation/process_metrics/include/test_lib.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include "opentelemetry/nostd/shared_ptr.h"
+#include "opentelemetry/metrics/async_instruments.h"
 class TestLib
 {
 public:
@@ -10,9 +12,19 @@ public:
   static void create_process_cpu_utilization_observable_gauge();
   static void create_process_memory_usage_observable_gauge();
   static void create_process_memory_virtual_observable_gauge();
-  static void create_process_disk_io_observable_counter();
-  static void create_process_network_io_observable_counter();
-  static void create_process_threads_observable_counter();
+  static void create_process_disk_io_observable_gauge();
+  static void create_process_network_io_observable_gauge();
+  static void create_process_threads_observable_gauge();
   static void create_process_open_files_observable_gauge();
-  static void create_process_context_switches_observable_counter();
+  static void create_process_context_switches_observable_gauge();
+private:
+  static opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> cpu_time_obserable_counter_;
+  static opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> cpu_utilization_obserable_gauge_;
+  static opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> memory_usage_obserable_gauge_;
+  static opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> memory_virtual_obserable_gauge_;
+  static opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> disk_io_obserable_gauge_;
+  static opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> network_io_obserable_gauge_;
+  static opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> threads_obserable_gauge_;
+  static opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> open_files_obserable_gauge_;
+  static opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> context_switches_obserable_gauge_;
 };
--- instrumentation/process_metrics/src/test_lib.cc
+++ instrumentation/process_metrics/src/test_lib.cc
@@ -3,78 +3,92 @@
 #  include "opentelemetry/context/context.h"
 #  include "opentelemetry/metrics/provider.h"
 
+
+#include <opentelemetry/sdk/metrics/async_instruments.h>
+#include <iostream>
 namespace nostd       = opentelemetry::nostd;
 namespace metrics_api = opentelemetry::metrics;
+
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> TestLib::cpu_time_obserable_counter_;
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> cpu_utilization_obserable_gauge_;
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> memory_usage_obserable_gauge_;
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> memory_virtual_obserable_gauge_;
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> disk_io_obserable_gauge_;
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> network_io_obserable_gauge_;
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> threads_obserable_gauge_;
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> open_files_obserable_gauge_;
+opentelemetry::nostd::shared_ptr<opentelemetry::metrics::ObservableInstrument> context_switches_obserable_gauge_;
 
 void TestLib::create_process_cpu_time_observable_counter()
 {
   auto provider                               = metrics_api::Provider::GetMeterProvider();
-  nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter("process.metrics", "1.2.0");
-  auto obserable_counter = meter->CreateLongObservableCounter("process.cpu.time", "des", "unit");
-  obserable_counter->AddCallback(ProcessMetricsFactory::GetProcessCpuTime, nullptr);
+  nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter("process.metrics", "1.2.0", "schema");
+  cpu_time_obserable_counter_ = meter->CreateLongObservableCounter("process.cpu.time", "des", "unit");
+  cpu_time_obserable_counter_->AddCallback(ProcessMetricsFactory::GetProcessCpuTime, nullptr);
+  auto storage = static_cast<opentelemetry::sdk::metrics::ObservableInstrument *>(cpu_time_obserable_counter_.get())->GetMetricStorage();
 }
 
 void TestLib::create_process_cpu_utilization_observable_gauge()
 {
   auto provider                               = metrics_api::Provider::GetMeterProvider();
   nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter("process.metrics", "1.2.0");
-  auto obserable_gauge = meter->CreateLongObservableGauge("process.cpu.utilization", "des", "unit");
-  obserable_gauge->AddCallback(ProcessMetricsFactory::GetProcessCpuUtilization, nullptr);
+  cpu_utilization_obserable_gauge_ = meter->CreateLongObservableGauge("process.cpu.utilization", "des", "unit");
+  cpu_utilization_obserable_gauge_->AddCallback(ProcessMetricsFactory::GetProcessCpuUtilization, nullptr);
 }
 
 void TestLib::create_process_memory_usage_observable_gauge()
 {
   auto provider                               = metrics_api::Provider::GetMeterProvider();
   nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter("process.metrics", "1.2.0");
-  auto obserable_gauge = meter->CreateLongObservableGauge("process.memory.usage", "des", "unit");
-  obserable_gauge->AddCallback(ProcessMetricsFactory::GetProcessMemoryUsage, nullptr);
+  memory_usage_obserable_gauge_ = meter->CreateLongObservableGauge("process.memory.usage", "des", "unit");
+  memory_usage_obserable_gauge_->AddCallback(ProcessMetricsFactory::GetProcessMemoryUsage, nullptr);
 }
 
 void TestLib::create_process_memory_virtual_observable_gauge()
 {
   auto provider                               = metrics_api::Provider::GetMeterProvider();
   nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter("process.metrics", "1.2.0");
-  auto obserable_gauge = meter->CreateLongObservableGauge("process.memory.virtual", "des", "unit");
-  obserable_gauge->AddCallback(ProcessMetricsFactory::GetProcessMemoryVirtual, nullptr);
+  memory_virtual_obserable_gauge_ = meter->CreateLongObservableGauge("process.memory.virtual", "des", "unit");
+  memory_virtual_obserable_gauge_->AddCallback(ProcessMetricsFactory::GetProcessMemoryVirtual, nullptr);
 }
 
-void TestLib::create_process_disk_io_observable_counter()
+void TestLib::create_process_disk_io_observable_gauge()
 {
   auto provider                               = metrics_api::Provider::GetMeterProvider();
   nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter("process.metrics", "1.2.0");
-  auto obserable_gauge = meter->CreateLongObservableGauge("process.disk.io", "des", "unit");
-  obserable_gauge->AddCallback(ProcessMetricsFactory::GetProcessDiskIO, nullptr);
+  disk_io_obserable_gauge_ = meter->CreateLongObservableGauge("process.disk.io", "des", "unit");
+  disk_io_obserable_gauge_->AddCallback(ProcessMetricsFactory::GetProcessDiskIO, nullptr);
 }
 
-void TestLib::create_process_network_io_observable_counter()
+void TestLib::create_process_network_io_observable_gauge()
 {
   auto provider                               = metrics_api::Provider::GetMeterProvider();
   nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter("process.metrics", "1.2.0");
-  auto obserable_gauge = meter->CreateLongObservableGauge("process.network.io", "des", "unit");
-  obserable_gauge->AddCallback(ProcessMetricsFactory::GetProcessNetworkIO, nullptr);   
+  network_io_obserable_gauge_ = meter->CreateLongObservableGauge("process.network.io", "des", "unit");
+  network_io_obserable_gauge_->AddCallback(ProcessMetricsFactory::GetProcessNetworkIO, nullptr);   
 }
 
-void TestLib::create_process_threads_observable_counter()
+void TestLib::create_process_threads_observable_gauge()
 {
   auto provider                               = metrics_api::Provider::GetMeterProvider();
   nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter("process.metrics", "1.2.0");
-  auto obserable_gauge = meter->CreateLongObservableGauge("process.threads", "des", "unit");
-  obserable_gauge->AddCallback(ProcessMetricsFactory::GetProcessThreads, nullptr);
+  threads_obserable_gauge_ = meter->CreateLongObservableGauge("process.threads", "des", "unit");
+  threads_obserable_gauge_->AddCallback(ProcessMetricsFactory::GetProcessThreads, nullptr);
 }
 
 void TestLib::create_process_open_files_observable_gauge()
 {
   auto provider                               = metrics_api::Provider::GetMeterProvider();
   nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter("process.metrics", "1.2.0");
-  auto obserable_gauge = meter->CreateLongObservableGauge("process.open.files", "des", "unit");
-  obserable_gauge->AddCallback(ProcessMetricsFactory::GetProcessOpenFileDescriptors, nullptr);
+  open_files_obserable_gauge_ = meter->CreateLongObservableGauge("process.open.files", "des", "unit");
+  open_files_obserable_gauge_->AddCallback(ProcessMetricsFactory::GetProcessOpenFileDescriptors, nullptr);
 }
 
-void TestLib::create_process_context_switches_observable_counter()
+void TestLib::create_process_context_switches_observable_gauge()
 {
   auto provider                               = metrics_api::Provider::GetMeterProvider();
   nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter("process.metrics", "1.2.0");
-  auto obserable_gauge = meter->CreateLongObservableGauge("process.context.switches", "des", "unit");
-  obserable_gauge->AddCallback(ProcessMetricsFactory::GetProcessContextSwitches, nullptr);   
+  context_switches_obserable_gauge_ = meter->CreateLongObservableGauge("process.context.switches", "des", "unit");
+  context_switches_obserable_gauge_->AddCallback(ProcessMetricsFactory::GetProcessContextSwitches, nullptr);   
 
 }
