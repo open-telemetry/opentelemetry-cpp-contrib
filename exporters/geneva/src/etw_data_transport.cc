4e661e01bd6de091bab02be24fabeb172157af18
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2022-11-02 19:28:53 -0700

    fix

--- exporters/geneva/src/etw_data_transport.cc
+++ exporters/geneva/src/etw_data_transport.cc
@@ -2,9 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "opentelemetry/exporters/geneva/metrics/unix_domain_socket_data_transport.h"
+#include "opentelemetry/exporters/geneva/metrics/uuid.h"
+
 #include "opentelemetry/exporters/geneva/metrics/macros.h"
 
 ETWDataTransport::ETWDataTransport(const std::string &etw_provider)
 {
+    UUID guid = (etw_provider.rfind("{", 0) == 0) 
+            ? UUID(etw_provider.c_str()) /// It's a ProviderGUID 
+            : GetProviderGuid(etw_provider.c_str());  // It's a ProviderName
     
 }
