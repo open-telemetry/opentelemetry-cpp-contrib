c683ba22257bbe3f7fe88c979959815c1918a380
Nick Anderson <105019436+niande-xbox@users.noreply.github.com>
2023-06-20 16:24:50 -0700

    Add const char* alternative for AttributeValueToString.

--- exporters/fluentd/include/opentelemetry/exporters/fluentd/common/fluentd_common.h
+++ exporters/fluentd/include/opentelemetry/exporters/fluentd/common/fluentd_common.h
@@ -110,6 +110,8 @@ inline std::string AttributeValueToString(
     result = std::to_string(nostd::get<uint64_t>(value));
   } else if (nostd::holds_alternative<double>(value)) {
     result = std::to_string(nostd::get<double>(value));
+  } else if (nostd::holds_alternative<const char*>(value)) {
+    result = std::string(nostd::get<const char*>(value));
   } else if (nostd::holds_alternative<opentelemetry::v1::nostd::string_view>(value)) {
     result = std::string(nostd::get<opentelemetry::v1::nostd::string_view>(value).data());
   } else {
