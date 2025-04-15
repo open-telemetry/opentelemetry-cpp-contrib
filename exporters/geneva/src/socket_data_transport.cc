12a60a7009c900797ca0609e9f611d640da4548e
Tom Tan <Tom.Tan@microsoft.com>
2025-04-15 15:05:42 -0700

    [Geneva] Do not set shutdown on connection failure in initialization (#545)

--- exporters/geneva/src/socket_data_transport.cc
+++ exporters/geneva/src/socket_data_transport.cc
@@ -58,7 +58,7 @@ bool SocketDataTransport::Send(MetricsEventType event_type,
           "Geneva Exporter: UDS::Send Socket reconnect failed. Send failed");
     }
   }
-  if (error_code != 0) {
+  if (!connected_ || error_code != 0 ) {
     LOG_ERROR("Geneva Exporter: UDS::Send failed - not connected");
     connected_ = false;
     return false;
