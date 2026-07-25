bd21cad38c9c7a51ae65f8ac30f1fc5fec06490b
proost <jwalag87@gmail.com>
2026-07-25 23:12:53 +0900

    chore: change lifecycle log severity to info from error

--- instrumentation/otel-webserver-module/src/nginx/ngx_http_opentelemetry_module.c
+++ instrumentation/otel-webserver-module/src/nginx/ngx_http_opentelemetry_module.c
@@ -813,7 +813,7 @@ static ngx_int_t ngx_http_opentelemetry_init_worker(ngx_cycle_t *cycle)
     int p = getpid();
     char * s = (char *)ngx_pcalloc(cycle->pool, 6);
     sprintf(s, "%d", p);
-    ngx_log_error(NGX_LOG_ERR, cycle->log, 0, "mod_opentelemetry: ngx_http_opentelemetry_init_worker: Initializing Nginx Worker for process with PID: %s", s);
+    ngx_log_error(NGX_LOG_INFO, cycle->log, 0, "mod_opentelemetry: ngx_http_opentelemetry_init_worker: Initializing Nginx Worker for process with PID: %s", s);
 
     /* Allocate memory for worker configuration */
     worker_conf = ngx_pcalloc(cycle->pool, sizeof(ngx_http_opentelemetry_worker_conf_t));
@@ -834,7 +834,7 @@ static void ngx_http_opentelemetry_exit_worker(ngx_cycle_t *cycle)
 {
     if (worker_conf && worker_conf->isInitialized)
     {
-        ngx_log_error(NGX_LOG_ERR, cycle->log, 0, "mod_opentelemetry: ngx_http_opentelemetry_exit_worker: Exiting Nginx Worker for process with PID: %s**********", worker_conf->pid);
+        ngx_log_error(NGX_LOG_INFO, cycle->log, 0, "mod_opentelemetry: ngx_http_opentelemetry_exit_worker: Exiting Nginx Worker for process with PID: %s**********", worker_conf->pid);
     }
 }
 
