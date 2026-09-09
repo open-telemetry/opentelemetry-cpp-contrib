cca68aa86b2c522cc31b57f8e53adfbef9f7e665
proost <jwalag87@gmail.com>
2026-09-10 00:03:29 +0900

    refactor: change to use renovate

--- cmake/opentelemetry-cpp.cmake
+++ cmake/opentelemetry-cpp.cmake
@@ -3,12 +3,6 @@
 
 include_guard(GLOBAL)
 
-if(NOT opentelemetry-cpp_GIT_TAG)
-  set(opentelemetry-cpp_GIT_TAG
-      "v1.28.0"
-      CACHE STRING "opentelemetry-cpp git tag to build when none is installed")
-endif()
-
 find_package(opentelemetry-cpp CONFIG QUIET)
 set(opentelemetry-cpp_PROVIDER "find_package")
 
@@ -24,7 +18,7 @@ if(NOT opentelemetry-cpp_FOUND)
   FetchContent_Declare(
     opentelemetry-cpp
     GIT_REPOSITORY "https://github.com/open-telemetry/opentelemetry-cpp.git"
-    GIT_TAG "${opentelemetry-cpp_GIT_TAG}"
+    GIT_TAG 6f65c3eded2bb4656c8d675cad43bbe694c531c7 # v1.28.0
     GIT_SHALLOW TRUE)
   set(opentelemetry-cpp_PROVIDER "fetch_repository")
 
@@ -36,9 +30,6 @@ if(NOT opentelemetry-cpp_FOUND)
     set(BUILD_TESTING "${_otel_cpp_prev_build_testing}" CACHE BOOL "" FORCE)
   endif()
   unset(_otel_cpp_prev_build_testing)
-
-  string(REGEX REPLACE "^v([0-9]+\\.[0-9]+\\.[0-9]+)$" "\\1" opentelemetry-cpp_VERSION
-                       "${opentelemetry-cpp_GIT_TAG}")
 endif()
 
 if(NOT TARGET opentelemetry-cpp::api)
